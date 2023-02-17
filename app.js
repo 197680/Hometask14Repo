7f11f5495a1d62617639b9b6660b031a37fdd076
197680 <103760394+197680@users.noreply.github.com>
2023-02-17 09:43:48 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+/*let quotationMarkInstruction = 'You can ignore single and double quotes like this:\\\'\\\'\\\"\\\"'
+console.log(quotationMarkInstruction)*/
+
+/*alert(`Please, enter your data`);
+let userName = prompt('Enter your name', 'name');
+alert(`Your name is: ${userName}`);
+let userSurname = prompt('Enter your surname', 'surname');
+alert(`Your full name is: ${userName + " " + userSurname}`);
+let userAge = prompt('How old are you?', 'Age');
+alert(`Your full name is: ${userName + " " + userSurname}, your age is: ${userAge}`);
+let userAdmin = confirm('Are you admin?');
+alert(`Your full name is: ${userName + " " + userSurname}, your age is: ${userAge}, admin status is: ${userAdmin}`);*/
+
